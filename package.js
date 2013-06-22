166e128c458f8cdd221ddffd0690aba619d84eb7
Michael Layzell <michael@redrasor.com>
2013-06-22 14:31:22 -0700

    First Commit

---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: 'Simple in-project depencency manager'
+});
+
+Package.on_use(function(api) {
+  api.use('underscore', ['client', 'server']);
+
+  api.add_files('require.js', ['client', 'server']);
+});
