a6c28d7cfc2a3e7ae836be28be3202e2774e0a6e
Michael Layzell <michael@redrasor.com>
2013-09-12 10:29:56 -0400

    Update to Meteor 0.6.5

--- package.js
+++ package.js
@@ -6,4 +6,8 @@ Package.on_use(function(api) {
   api.use(['meteor', 'underscore'], ['client', 'server']);
 
   api.add_files('module.js', ['client', 'server']);
+
+  if (typeof api.export !== 'undefined') {
+    api.export(['module'], ['client', 'server']);
+  }
 });
